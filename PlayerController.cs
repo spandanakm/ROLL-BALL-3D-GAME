37d02de5c245ae1c682351952452f0c664a9ce90
spandanakm <155885806+spandanakm@users.noreply.github.com>
2025-05-14 08:20:21 +0530

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed;
+    Rigidbody rb;
+    
+    float xInput;
+    float yInput;
+    int score = 0;
+    public int winScore;
+
+    public GameObject winText;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -5f)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+
+        rb.AddForce(xInput * speed, 0, yInput * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Coin")
+        {
+            other.gameObject.SetActive(false);
+
+            score++;
+            if(score >= winScore)
+            {
+                winText.SetActive(true);
+            }
+        }
+    }
+}
